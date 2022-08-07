264e67a52fca844a40949b24da8b67bc6ddd3c94
Artyom Lubinets <artyom.lubinetsa@gmail.com>
2022-08-08 02:53:18 +0600

    задачи за 5 модуль

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,28 @@
 function initCarousel() {
   // ваш код...
+  let carouselInner = document.querySelector(".carousel__inner");
+  let slideWidth = document.querySelector(".carousel__inner").offsetWidth;
+  let nextSlide = document.querySelector('.carousel__arrow_right');
+  let prevSlide = document.querySelector('.carousel__arrow_left');
+
+  let counter = 0;
+
+  carouselArrowDisplay = () => {
+    counter > 2 ? nextSlide.style.display = 'none' : nextSlide.style.display = '';
+    counter > 0 ? prevSlide.style.display = '' : prevSlide.style.display = 'none';
+  }
+
+  carouselArrowDisplay();
+  
+  nextSlide.addEventListener("click", () => {
+    counter++;
+    carouselInner.style.transform = 'translateX(' + (-slideWidth * counter) + 'px)';
+    carouselArrowDisplay();
+  })
+
+  prevSlide.addEventListener("click", () => {
+    counter--;
+    carouselInner.style.transform = 'translateX(' + (-slideWidth * counter) + 'px)';
+    carouselArrowDisplay();
+})
 }
